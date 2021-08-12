32627ae0c11d722557b20c1cd54c7027b24987e0
domwitt21 <60725257+domwitt21@users.noreply.github.com>
2021-08-11 21:24:52 -0400

    One Love Photography

--- site/js/home.js
+++ site/js/home.js
@@ -2,11 +2,3 @@ function navBar() {
     var x = document.getElementById("mobMenu");
     x.classList.toggle("mobMenuOpen");
 }
-
-// carousel script start
-
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 2000,
-    wrap: false
-})
